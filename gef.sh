43ca32dd54941085f02062405a54d041c5476b85
crazy rabbidz <hugsy@users.noreply.github.com>
2015-04-05 14:27:13 +1000

    Update gef.sh

--- gef.sh
+++ gef.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PATH="/sbin:/usr/bin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/bin"
 
 curl -s -o "$HOME/.gdbinit-gef.py" -L https://github.com/hugsy/gef/raw/master/gef.py
 echo "source $HOME/.gdbinit-gef.py" >> "$HOME/.gdbinit"
