270f568b64178df510ec1573e6eb0a38ba8d27e8
Grazfather <Grazfather@gmail.com>
2017-04-23 13:16:46 -0700

    Cast registers to unsigned in get_register
    GDB types registers, meaning $esp and $eax, for example, can have the
    same value, but evaluate to different values (eax being negative, esp
    always unsigned). This casts the value to an unsigned.
    
    This fixes DereferenceCommand not showing when other registers point to
    an address.

--- gef.py
+++ gef.py
@@ -1846,7 +1846,7 @@ def catch_generic_exception(f):
 
 
 def to_unsigned_long(v):
-    """Helper to cast a gdb.Value to unsigned long."""
+    """Cast a gdb.Value to unsigned long."""
     unsigned_long_t = cached_lookup_type("unsigned long")
     return long(v.cast(unsigned_long_t))
 
@@ -1856,6 +1856,8 @@ def get_register(regname):
     regname = regname.strip()
     try:
         value = gdb.parse_and_eval(regname)
+        if value.type.code == gdb.TYPE_CODE_INT:
+           value = to_unsigned_long(value)
         return long(value)
     except gdb.error:
         value = gdb.selected_frame().read_register(regname)
