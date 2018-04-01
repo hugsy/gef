adb50d89bf39ee61ed418e852839caa70ae189a5
crazy rabbidz <hugsy@users.noreply.github.com>
2018-03-31 23:52:12 -0700

    Update run-remote.sh

--- tests/run-remote.sh
+++ tests/run-remote.sh
@@ -4,21 +4,21 @@
 # (assumes GDB+Python3, requires sudo NOPASSWD)
 #
 # On the host, run:
-# $ sh -c "$(curl https://raw.githubusercontent.com/hugsy/gef/master/tests/run-remote.sh)"
+# $ bash -c "$(curl https://raw.githubusercontent.com/hugsy/gef/master/tests/run-remote.sh)"
 #
 
 set -e
 
 pushd .
-rm -fr /tmp/gef-tests
+rm -fr /tmp/gef-tests && mkdir /tmp/gef-tests
 cd /tmp/gef-tests
 sudo apt update
 sudo apt upgrade -y
-sudo apt install gdb git cmake gcc g++ pkg-config libglib2.0-dev python3-pip python3-setuptools python3-dev python3 make -y
+sudo apt install gdb git cmake gcc g++ pkg-config libglib2.0-dev libssl-dev python3-pip python3-setuptools python3-dev python3 make -y
 sudo rm -fr -- /tmp/{keystone,capstone,unicorn}
 sh -c "$(curl https://raw.githubusercontent.com/hugsy/stuff/master/update-trinity.sh)"
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-pip3 install -U ropper cryptography
+sudo pip3 install -U ropper cryptography
 git clone https://github.com/hugsy/gef.git
 cd gef
 tests/test-runner.py
