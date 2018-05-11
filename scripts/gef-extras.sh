7ec892abfa0e03f1e4c1a75b85f1ede7b73c07c6
crazy rabbidz <hugsy@users.noreply.github.com>
2018-05-11 14:07:07 -0700

    Update gef-extras.sh

--- scripts/gef-extras.sh
+++ scripts/gef-extras.sh
@@ -1,2 +1,18 @@
-git clone https://github.com/hugsy/gef-extras.git $HOME/gef-extras
-gdb -q -ex 'gef config gef.extra_plugins_dir "'$HOME'/gef-extras/scripts"' -ex 'gef config pcustom.struct_path '$HOME'/gef-extras/structs' -ex 'gef save' -ex quit
+#!/bin/bash
+
+set -e
+
+if [ $# -ge 1 ]; then
+  DIR="$(realpath $1)"
+  test -d ${DIR} || exit 1
+else
+  DIR=${HOME}
+fi
+
+git clone https://github.com/hugsy/gef-extras.git ${DIR}/gef-extras
+gdb -q -ex "gef config gef.extra_plugins_dir '${DIR}/gef-extras/scripts'" \
+       -ex "gef config pcustom.struct_path '${DIR}/gef-extras/structs'" \
+       -ex 'gef save' \
+       -ex quit
+
+exit 0
