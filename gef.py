553f718c214c3c49410230983ba4afc13654f998
Kyle ZENG <jkjh1jkjh1@gmail.com>
2018-10-07 17:27:26 -0700

    fallback to read memory when gdb api fails to retrieve a cstring

--- gef.py
+++ gef.py
@@ -2172,7 +2172,12 @@ def read_cstring_from_memory(address, max_length=GEF_MAX_STRING_LENGTH, encoding
 
     char_t = cached_lookup_type("char")
     char_ptr = char_t.pointer()
-    res = gdb.Value(address).cast(char_ptr).string(encoding=encoding).strip()
+    try:
+        res = gdb.Value(address).cast(char_ptr).string(encoding=encoding).strip()
+    except gdb.error:
+        length = min(address|(DEFAULT_PAGE_SIZE-1), max_length+1)
+        mem = read_memory(address, length).decode('utf-8')
+        res = mem.split('\x00', 1)[0]
     res2 = res.replace('\n','\\n').replace('\r','\\r').replace('\t','\\t')
 
     if max_length and len(res) > max_length:
