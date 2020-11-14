987010b9a7a48912fc5b07f30b77f593f02edc2d
guly <theguly@users.noreply.github.com>
2020-11-14 17:52:08 +0100

    added glibc args to gef-extras install script

--- scripts/gef-extras.sh
+++ scripts/gef-extras.sh
@@ -13,6 +13,8 @@ git clone https://github.com/hugsy/gef-extras.git ${DIR}/gef-extras
 gdb -q -ex "gef config gef.extra_plugins_dir '${DIR}/gef-extras/scripts'" \
        -ex "gef config pcustom.struct_path '${DIR}/gef-extras/structs'" \
        -ex "gef config syscall-args.path '${DIR}/gef-extras/syscall-tables'" \
+       -ex "gef config libc_args True" \
+       -ex "gef config libc_args_path '${DIR}/gef-extras/glibc-function-args'" \
        -ex 'gef save' \
        -ex quit
 
