0f7a5e5ce2f7724eb27a0f987d3670d9fb672ffd
Michael Hoefler <hoefler2002@gmail.com>
2021-06-08 23:15:18 -0400

    fix alias tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -523,14 +523,13 @@ class TestGefCommands(GefUnitTestGeneric): #pylint: disable=too-many-public-meth
         add_res = gdb_start_silent_cmd("aliases add alias_function_test example")
         self.assertNoException(add_res)
         # test list functionality
-        list_res = gdb_start_silent_cmd("aliases ls")
+        list_res = gdb_start_silent_cmd("aliases ls", before=["aliases add alias_function_test example"])
         self.assertNoException(list_res)
         self.assertIn("alias_function_test", list_res)
         # test rm functionality
-        rm_res = gdb_start_silent_cmd("aliases rm alias_function_test")
+        rm_res = gdb_start_silent_cmd("aliases ls", before=["aliases add alias_function_test example", "aliases rm alias_function_test"])
         self.assertNoException(rm_res)
-        rm_list_res = gdb_start_silent_cmd("aliases ls")
-        self.assertNotIn("alias_function_test", rm_list_res)
+        self.assertNotIn("alias_function_test", rm_res)
         return
 
 
