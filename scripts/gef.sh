dab4137fa9c4e957c354f5b7e18ca3cf4890ccc3
Simon Segars <BadSugar@users.noreply.github.com>
2021-08-10 23:19:17 +0300

    gef.sh: check if cURL or wget exist on the system

--- scripts/gef.sh
+++ scripts/gef.sh
@@ -9,16 +9,35 @@ if [ "$1" = "dev" ]; then
     echo "set branch to dev"
 fi
 
+# check dependencies
+if [ `which curl` ]; then
+	curl_found=1
+elif [ `which wget` ]; then
+	wget_found=1
+else
+	echo "Please install cURL or wget and run again"
+	exit 1
+fi
+
 # Backup gdbinit if any
 if [ -f "${HOME}/.gdbinit" ]; then
     mv "${HOME}/.gdbinit" "${HOME}/.gdbinit.old"
 fi
 
-# Get the hash of the commit
-ref=$(wget -q -O- https://api.github.com/repos/hugsy/gef/git/ref/heads/${branch} | grep '"sha"' | tr -s ' ' | cut -d ' ' -f 3 | tr -d "," | tr -d '"')
+if [ $wget_found -eq 1 ]; then
+    # Get the hash of the commit
+    ref=$(wget -q -O- https://api.github.com/repos/hugsy/gef/git/ref/heads/${branch} | grep '"sha"' | tr -s ' ' | cut -d ' ' -f 3 | tr -d "," | tr -d '"')
 
-# Download the file
-wget -q "https://github.com/hugsy/gef/raw/${branch}/gef.py" -O "${HOME}/.gef-${ref}.py"
+    # Download the file
+    wget -q "https://github.com/hugsy/gef/raw/${branch}/gef.py" -O "${HOME}/.gef-${ref}.py"
+elif [ $curl_found -eq 1 ]; then
+    # Get the hash of the commit
+    ref=$(curl --silent https://api.github.com/repos/hugsy/gef/git/ref/heads/${branch} | grep '"sha"' | tr -s ' ' | cut -d ' ' -f 3 | tr -d "," | tr -d '"')
+
+    # Download the file
+    curl --silent --location --output "${HOME}/.gef-${ref}.py" "https://github.com/hugsy/gef/raw/${branch}/gef.py"
+fi
+
 # Create the new gdbinit
 echo "source ~/.gef-${ref}.py" > ~/.gdbinit
 
