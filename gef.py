e1696635b568396abc7de068c300ad8ae1c09a9d
theguy <37738506+theguy147@users.noreply.github.com>
2021-12-14 14:31:58 +0100

    refactor: remove utf8 encoding declaration

--- gef.py
+++ gef.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-#
 #######################################################################################
 # GEF - Multi-Architecture GDB Enhanced Features for Exploiters & Reverse-Engineers
 #
