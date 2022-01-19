b2870003a17c0fdf0dad094314a38af583a0bfaa
theguy <37738506+theguy147@users.noreply.github.com>
2022-01-19 11:40:31 +0100

    remove unused import of AnyStr

--- gef.py
+++ gef.py
@@ -82,7 +82,7 @@ import xmlrpc.client as xmlrpclib
 from functools import lru_cache
 from io import StringIO, TextIOWrapper
 from types import ModuleType
-from typing import (Any, AnyStr, ByteString, Callable, Dict, Generator, IO, Iterator, List,
+from typing import (Any, ByteString, Callable, Dict, Generator, IO, Iterator, List,
                     NoReturn, Optional, Sequence, Set, Tuple, Type, Union)
 from urllib.request import urlopen
 
