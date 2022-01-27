a39f211b302ad9c281aa7170e9b510b9f9d2244a
hugsy <hugsy@blah.cat>
2022-01-26 17:19:15 -0800

    [ci] added tests for `nop`

--- tests/commands/nop.py
+++ tests/commands/nop.py
@@ -2,8 +2,9 @@
 `nop` command test module
 """
 
+import pytest
 
-from tests.utils import GefUnitTestGeneric, gdb_run_cmd
+from tests.utils import ARCH, GefUnitTestGeneric, findlines, gdb_run_cmd, gdb_start_silent_cmd
 
 
 class NopCommand(GefUnitTestGeneric):
@@ -13,6 +14,37 @@ class NopCommand(GefUnitTestGeneric):
     cmd = "nop"
 
 
-    def test_cmd_nop(self):
+    def test_cmd_nop_inactive(self):
         res = gdb_run_cmd(f"{self.cmd}")
+        self.assertFailIfInactiveSession(res)
+
+
+    @pytest.mark.skipif(ARCH not in ("i686", "x86_64"), reason=f"Skipped for {ARCH}")
+    def test_cmd_nop_no_arg(self):
+        res = gdb_start_silent_cmd(
+            "pi print(f'*** *pc={u8(gef.memory.read(gef.arch.pc, 1))}')",
+            after=(
+                self.cmd,
+                "pi print(f'*** *pc={u8(gef.memory.read(gef.arch.pc, 1))}')",
+            )
+        )
         self.assertNoException(res)
+        lines = findlines("*** *pc=", res)
+        self.assertEqual(len(lines), 2)
+        self.assertEqual(lines[1], "*** *pc=144") # nop -> 0x90 -> 144
+
+
+    @pytest.mark.skipif(ARCH not in ("i686", "x86_64"), reason=f"Skipped for {ARCH}")
+    def test_cmd_nop_arg(self):
+        res = gdb_start_silent_cmd(
+            "pi print(f'*** *sp={u32(gef.memory.read(gef.arch.sp, 4))}')",
+            after=(
+                f"{self.cmd} $sp --nb 4",
+                "pi print(f'*** *sp={u32(gef.memory.read(gef.arch.sp, 4))}')",
+            )
+        )
+        self.assertNoException(res)
+        lines = findlines("*** *sp=", res)
+        self.assertEqual(len(lines), 2)
+        self.assertEqual(lines[1], "*** *sp=2425393296") # 4*nop -> 0x90909090 -> 2425393296
+
