d1fa00fcdb7f81a99cc979c0a197405e2d02b1f7
theguy147 <37738506+theguy147@users.noreply.github.com>
2022-02-01 19:28:30 +0100

    fix: make shebang lines portable (#814)
    * fix: make shebang lines portable
    
    * fix: SC2006, SC2086, SC2016, SC2059

--- tests/perf/context_times.sh
+++ tests/perf/context_times.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Bail out early on an unexpected error
 set -e
@@ -17,13 +17,13 @@ get_context_time() {
 }
 
 log_this_revision() {
-    rev=`git log -1 --pretty="format:%h"`
-    printf $rev
-    title=`git log -1 --pretty="format:%s"`
-    printf ",\"$title\""
-    for i in `seq 1 5`; do
-        rv=`$1`
-        printf ",$rv"
+    rev=$(git log -1 --pretty="format:%h")
+    printf "%s" "$rev"
+    title=$(git log -1 --pretty="format:%s")
+    printf ",\"%s\"" "$title"
+    for _ in $(seq 1 5); do
+        rv=$($1)
+        printf ",%s" "$rv"
     done
     printf "\n"
 }
@@ -39,7 +39,7 @@ clear_stats() {
 }
 
 get_current_branch_or_commit() {
-    b=`git branch | grep '\* '`
+    b=$(git branch | grep '\* ')
     case "$b" in
         *HEAD*)
             echo "$b" | tr -d ')' | cut -f 5 -d ' ';;
@@ -53,23 +53,23 @@ run_on_git_revisions() {
     end_ref=$2
     test_command=$3
 
-    orig_rev=`get_current_branch_or_commit`
-    revs=`git rev-list --reverse ${start_ref}..${end_ref}`
+    orig_rev=$(get_current_branch_or_commit)
+    revs=$(git rev-list --reverse "${start_ref}".."${end_ref}")
 
     for rev in $revs; do
-        echo "Checking out: $(git log --oneline -1 $rev)"
-        git checkout --quiet $rev
-        log_command $test_command
+        echo "Checking out: $(git log --oneline -1 "$rev")"
+        git checkout --quiet "$rev"
+        log_command "$test_command"
         git reset --hard --quiet
     done
 
     echo "Restoring original commit/branch: $orig_rev"
-    git checkout --quiet $orig_rev
+    git checkout --quiet "$orig_rev"
 }
 
 if [ $# == "2" ]; then
     clear_stats
-    run_on_git_revisions $1 $2 "time_gef_context"
+    run_on_git_revisions "$1" "$2" "time_gef_context"
 else
     echo "usage: $0 first_commit last_commit"
 fi
