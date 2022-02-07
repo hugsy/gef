af339c56e490a162ce3032405f66ae79502e0735
hugsy <hugsy@blah.cat>
2022-02-06 16:18:39 -0800

    [tests] `edit-flags` are only for known arches for now

--- tests/commands/edit_flags.py
+++ tests/commands/edit_flags.py
@@ -13,6 +13,8 @@ from tests.utils import (
 )
 
 
+@pytest.mark.skipif(ARCH not in ["i686", "x86_64", "armv7l", "aarch64"],
+                    reason=f"Skipped for {ARCH}")
 class EditFlagsCommand(GefUnitTestGeneric):
     """`edit-flags` command test module"""
 
@@ -20,13 +22,11 @@ class EditFlagsCommand(GefUnitTestGeneric):
         res = gdb_start_silent_cmd_last_line("edit-flags")
         self.assertNoException(res)
         flags = res[1:-1].split()
-        self.flag_name = "carry" # todo: update with other arches
+        self.flag_name = "carry"
         self.initial_value = [f for f in flags if f.lower() == self.flag_name][0]
         return super().setUp()
 
 
-    @pytest.mark.skipif(ARCH not in ["i686", "x86_64", "armv7l", "aarch64"],
-                        reason=f"Skipped for {ARCH}")
     def test_cmd_edit_flags_disable(self):
         res = gdb_start_silent_cmd_last_line("edit-flags",
                                              after=(f"edit-flags +{self.flag_name}",
@@ -35,8 +35,6 @@ class EditFlagsCommand(GefUnitTestGeneric):
         self.assertIn(self.flag_name.lower(), res)
 
 
-    @pytest.mark.skipif(ARCH not in ["i686", "x86_64", "armv7l", "aarch64"],
-                        reason=f"Skipped for {ARCH}")
     def test_cmd_edit_flags_enable(self):
         res = gdb_start_silent_cmd("edit-flags",
                                              after=(f"edit-flags -{self.flag_name}",
@@ -45,8 +43,6 @@ class EditFlagsCommand(GefUnitTestGeneric):
         self.assertIn(self.flag_name.upper(), res)
 
 
-    @pytest.mark.skipif(ARCH not in ["i686", "x86_64", "armv7l", "aarch64"],
-                        reason=f"Skipped for {ARCH}")
     def test_cmd_edit_flags_toggle(self):
         res = gdb_start_silent_cmd_last_line(f"edit-flags ~{self.flag_name}")
         self.assertNoException(res)
