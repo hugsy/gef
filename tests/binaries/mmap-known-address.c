d742aae775579c71f0864e0e21a409eee408e364
hugsy <hugsy@blah.cat>
2022-07-02 08:29:42 -0700

    [ci] changed the target binary for `nop` test

---
+++ tests/binaries/mmap-known-address.c
@@ -0,0 +1,34 @@
+/**
+ * -*- mode: c -*-
+ * -*- coding: utf-8 -*-
+ *
+ * mmap-known-address.c : only mmap() at 0x1337000 and DebugBreak
+ *
+ * @author: @_hugsy_
+ * @licence: WTFPL v.2
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include "utils.h"
+
+int main(int argc, char **argv, char **envp)
+{
+    void *p = mmap((void *)0x1337000,
+                   getpagesize(),
+                   PROT_READ | PROT_WRITE,
+                   MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED,
+                   -1,
+                   0);
+
+    if (p == (void *)-1)
+        return EXIT_FAILURE;
+
+    DebugBreak();
+
+    return EXIT_SUCCESS;
+}
