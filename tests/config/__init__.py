926dc3fd97b3ba26cfa6bff2613c1f947714df4e
r12f <r12f.code@gmail.com>
2023-12-20 23:28:09 +0000

    more tests.

--- tests/config/__init__.py
+++ tests/config/__init__.py
@@ -2,7 +2,7 @@
 Test GEF configuration parameters.
 """
 
-from tests.utils import gdb_run_cmd
+from tests.utils import gdb_run_cmd, gdb_start_silent_cmd
 from tests.utils import GefUnitTestGeneric
 
 
@@ -65,3 +65,8 @@ class TestGefConfigUnit(GefUnitTestGeneric):
         self.assertNoException(res)
         self.assertNotIn("[!]", res)
         self.assertIn('gef.libc_version (str) = ""', res)
+
+        res = gdb_start_silent_cmd("python print(gef.libc.version)", before=["gef config gef.libc_version 2.31"])
+        self.assertNoException(res)
+        self.assertNotIn("[!]", res)
+        self.assertIn('(2, 31)', res)
