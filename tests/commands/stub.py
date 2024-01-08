c9d7a92971d39f5b7c9b6dd03e93944e1f73a165
hugsy <crazy.hugsy@outlook.com>
2024-01-07 16:16:08 -0800

    test fixes 6/N

--- tests/commands/stub.py
+++ tests/commands/stub.py
@@ -3,12 +3,17 @@ stub command test module
 """
 
 
+import pytest
 from tests.base import RemoteGefUnitTestGeneric
 from tests.utils import ERROR_INACTIVE_SESSION_MESSAGE
 
 
 class StubCommand(RemoteGefUnitTestGeneric):
     """`stub` command test module"""
+
+    @pytest.fixture(autouse=True)
+    def capfd(self, capfd):
+        self.capfd = capfd
 
     def test_cmd_stub(self):
         gdb = self._gdb
@@ -19,12 +24,28 @@ class StubCommand(RemoteGefUnitTestGeneric):
                 ERROR_INACTIVE_SESSION_MESSAGE, gdb.execute(cmd, to_string=True)
             )
 
+        #
+        # Sanity Check - no exception
+        #
         gdb.execute("start")
-        res = gdb.execute("continue", to_string=True).strip()
-        self.assertIn("Hello World!", res)
+        captured = self.capfd.readouterr()
+        gdb.execute("continue")
+        captured = self.capfd.readouterr()
 
+        assert "Hello World!" in captured.out
+
+        #
+        # Make sure the prints are stubbed out
+        #
+        gdb.execute("start")
         for cmd in cmds:
-            gdb.execute("start")
-            res = gdb.execute(cmd, to_string=True)
-            self.assertNotIn("Hello World!", res)
-            gdb.execute("continue")
+            gdb.execute(f"{cmd} --retval 42")
+        assert len(gdb.breakpoints()) == len(cmds)
+
+        #
+        # Check again, make sure the stdout buffer is emptied
+        #
+        captured = self.capfd.readouterr()
+        gdb.execute("continue")
+        captured = self.capfd.readouterr()
+        assert "Hello World!" not in captured.out
