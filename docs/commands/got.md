0d9507d609d14ff0db054b257db7d1248ff75e57
Gordon Messmer <gordon.messmer@gmail.com>
2024-05-09 17:26:56 -0700

    Add an '--all' option to the got command for information about linked shared objects.

--- docs/commands/got.md
+++ docs/commands/got.md
@@ -6,8 +6,10 @@ The `got` command optionally takes function names and filters the output display
 matching functions.
 
 ```text
-gef➤ got
+gef➤ got [--all] [filters]
 ```
+
+`--all` Print the GOT for all shared objects in addition to the executable file
 
 ![gef-got](https://i.imgur.com/554ebM3.png)
 
@@ -28,3 +30,9 @@ gef➤ got str get
 ```
 
 ![gef-got-multi-filter](https://i.imgur.com/7L2uLt8.png)
+
+```text
+gef➤ got --all str get
+```
+
+Print relocatable symbols matching "str" or "get" in the executable and all shared object files.
--- tests/commands/got.py
+++ tests/commands/got.py
@@ -35,3 +35,36 @@ class GotCommand(RemoteGefUnitTestGeneric):
         res = gdb.execute("got printf", to_string=True)
         self.assertIn("printf", res)
         self.assertNotIn("strcpy", res)
+        self.assertNotIn("/libc", res)
+
+        def checksyms(lines):
+            if not lines:
+                return None
+            if "format-string-helper.out" in lines[0]:
+                res = ''.join(lines)
+                self.assertIn(" printf", res)
+                self.assertNotIn(" strcpy", res)
+                return "format-string-helper.out"
+            if "/libc" in lines[0]:
+                res = ''.join(lines)
+                self.assertNotIn(" printf", res)
+                self.assertNotIn(" strcpy", res)
+                return "libc"
+            return None
+
+        res = gdb.execute("got --all printf", to_string=True)
+        # Keep a list of output blocks describing files mapped in the process
+        checked_sections = []
+        # Iterate over lines of output and assemble blocks.  When a new block
+        # is found, or when the end of output is reached, check the output
+        # block for symbols expected in that block.
+        lines = []
+        for line in res.splitlines():
+            if line.startswith("─"):
+                checked_sections.append(checksyms(lines))
+                lines = []
+            lines.append(line)
+        checked_sections.append(checksyms(lines))
+        # Make sure that both the executable and libc sections were found.
+        self.assertIn("format-string-helper.out", checked_sections)
+        self.assertIn("libc", checked_sections)
