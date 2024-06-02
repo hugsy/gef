932f064d215f245a597722122983ebfef927fb97
ValekoZ <lucas.valentin88@gmail.com>
2024-06-02 18:05:31 +0200

    Add tests

---
+++ tests/commands/arch.py
@@ -0,0 +1,48 @@
+"""
+Arch commands test module
+"""
+
+from tests.base import RemoteGefUnitTestGeneric
+from tests.utils import (
+    ARCH,
+    ERROR_INACTIVE_SESSION_MESSAGE,
+    debug_target,
+    findlines,
+    is_32b,
+    is_64b,
+)
+
+
+class ArchCommand(RemoteGefUnitTestGeneric):
+    """Generic class for command testing, that defines all helpers"""
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_cmd_arch_get(self):
+        gdb = self._gdb
+
+        res = gdb.execute("arch get", to_string=True)
+        self.assertIn(" Architecture(Generic, None, LITTLE_ENDIAN)", res)
+        self.assertIn(" This default architecture", res)
+
+    def test_cmd_arch_set(self):
+        gdb = self._gdb
+
+        res = gdb.execute("arch get", to_string=True)
+        self.assertIn(" Architecture(Generic, None, LITTLE_ENDIAN)", res)
+        self.assertIn(" This default architecture", res)
+
+        gdb.execute("arch set X86")
+
+        res = gdb.execute("arch get", to_string=True)
+        self.assertIn(" Architecture(Generic, None, LITTLE_ENDIAN)", res)
+        self.assertIn(" The architecture has been set manually", res)
+
+    def test_cmd_arch_list(self):
+        gdb = self._gdb
+
+        res = gdb.execute("arch list", to_string=True)
+        self.assertIn("- GenericArchitecture", res)
+        self.assertIn("- X86", res)
+        self.assertIn("- X86_64", res)
