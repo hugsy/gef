7c326024d3119622e07353a2b9ef44183e51015b
ValekoZ <lucas.valentin@synacktiv.com>
2024-06-05 14:11:09 +0200

    Move activate_script_path

--- gef.py
+++ gef.py
@@ -9949,8 +9949,8 @@ class GefCommand(gdb.Command):
 
     def load_virtualenv(self, new_path: Optional[pathlib.Path] = None):
         path = new_path or gef.config["gef.virtualenv_path"]
-        activate_script_path = pathlib.Path(path) / "bin" / "activate_this.py"
         if path:
+            activate_script_path = pathlib.Path(path)/"bin"/"activate_this.py"
             exec(open(activate_script_path).read(), {'__file__': activate_script_path})
 
     @property
