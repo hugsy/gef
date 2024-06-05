65330a7a093ea95fbd288652fab39a84ca15d65c
ValekoZ <lucas.valentin@synacktiv.com>
2024-06-05 13:48:42 +0200

    Add support for virtualenv
    Add a `gef.virtualenv_path` config var, and activate it at startup.

--- gef.py
+++ gef.py
@@ -9854,6 +9854,9 @@ class GefCommand(gdb.Command):
         plugins_dir = GefSetting("", str, "Autoload additional GEF commands from external directory", hooks={"on_write": [GefSetting.no_spaces, ]})
         plugins_dir.add_hook("on_changed", [lambda _, new_val: GefSetting.must_exist(new_val), lambda _, new_val: self.load_extra_plugins(new_val), ])
         gef.config["gef.extra_plugins_dir"] = plugins_dir
+        venv_path = GefSetting("", str, "Path to the virtualenv used by GEF", hooks={"on_write": [GefSetting.no_spaces, ]})
+        venv_path.add_hook("on_changed", [lambda _, new_val: GefSetting.must_exist(new_val), lambda _, new_val: self.load_virtualenv(new_val), ])
+        gef.config["gef.virtualenv_path"] = venv_path
         gef.config["gef.disable_color"] = GefSetting(False, bool, "Disable all colors in GEF")
         gef.config["gef.tempdir"] = GefSetting(GEF_TEMP_DIR, pathlib.Path, "Directory to use for temporary/cache content", hooks={"on_write": [GefSetting.no_spaces, GefSetting.create_folder_tree]})
         gef.config["gef.show_deprecation_warnings"] = GefSetting(True, bool, "Toggle the display of the `deprecated` warnings")
@@ -9942,6 +9945,13 @@ class GefCommand(gdb.Command):
             return 0
         dbg(f"Loading extra plugins from directory={directory}")
         return load_plugins_from_directory(directory)
+
+
+    def load_virtualenv(self, new_path: Optional[pathlib.Path] = None):
+        path = new_path or pathlib.Path(gef.config["gef.virtualenv_path"])
+        activate_script_path = path / "bin" / "activate_this.py"
+        if path:
+            exec(open(activate_script_path).read(), {'__file__': activate_script_path})
 
     @property
     def loaded_command_names(self) -> Iterable[str]:
@@ -11716,6 +11726,9 @@ if __name__ == "__main__":
     assert isinstance(gef, Gef)
     gef.gdb.load()
     gef.gdb.show_banner()
+
+    # load venv
+    gef.gdb.load_virtualenv()
 
     # load config
     gef.gdb.load_extra_plugins()
