hugsy/gef

commit 29fb74efa29ec54296016d963724315e743689fa

add layout mapping API for external plugins (#1093)
## Description

This fixes all the issues with the context panes and it now works as it
did before the plugins were created.

In the GEF part, it was just a minor refactor and a new external API. 

In the gef-extras part, it was only necessary to call this new function!

I think the proposed solution is simple and it works.

Related to hugsy/gef-extras#111

gef.py
@@ -4550,6 +4550,10 @@ def register_external_context_pane(pane_name: str, display_pane_function: Callab
     gef.gdb.add_context_pane(pane_name, display_pane_function, pane_title_function, condition)
     return
 
+def register_external_context_layout_mapping(current_pane_name: str, display_pane_function: Callable[[], None], pane_title_function: Callable[[], Optional[str]], condition : Optional[Callable[[], bool]] = None) -> None:
+    gef.gdb.add_context_layout_mapping(current_pane_name, display_pane_function, pane_title_function, condition)
+    return
+
 
 #
 # Commands
@@ -9836,6 +9840,14 @@ class GefCommand(gdb.Command):
         gdb.execute("gef help")
         return
 
+    def add_context_layout_mapping(self, current_pane_name: str, display_pane_function: Callable, pane_title_function: Callable, condition: Optional[Callable]) -> None:
+        """Add a new context layout mapping."""
+        context = self.commands["context"]
+        assert isinstance(context, ContextCommand)
+
+        # overload the printing of pane title
+        context.layout_mapping[current_pane_name] = (display_pane_function, pane_title_function, condition)
+
     def add_context_pane(self, pane_name: str, display_pane_function: Callable, pane_title_function: Callable, condition: Optional[Callable]) -> None:
         """Add a new context pane to ContextCommand."""
         context = self.commands["context"]
@@ -9845,8 +9857,7 @@ class GefCommand(gdb.Command):
         corrected_settings_name: str = pane_name.replace(" ", "_")
         gef.config["context.layout"] += f" {corrected_settings_name}"
 
-        # overload the printing of pane title
-        context.layout_mapping[corrected_settings_name] = (display_pane_function, pane_title_function, condition)
+        add_context_layout_mapping(corrected_settings_name, display_pane_function, pane_title_function, condition)
 
     def load(self) -> None:
         """Load all the commands and functions defined by GEF into GDB."""

scripts/gef-extras.sh
@@ -38,7 +38,9 @@ fi
 git clone --branch ${branch} https://github.com/hugsy/gef-extras.git "${DIR}"
 ver=$(gdb -q -nx -ex 'pi print(f"{sys.version_info.major}.{sys.version_info.minor}", end="")' -ex quit)
 python${ver} -m pip install --requirement "${DIR}"/requirements.txt --upgrade
-gdb -q -ex "gef config gef.extra_plugins_dir '${DIR}/scripts'" \
+gdb -q -ex "pi gef.config['context.layout'] += ' syscall_args'" \
+       -ex "pi gef.config['context.layout'] += ' libc_function_args'" \
+       -ex "gef config gef.extra_plugins_dir '${DIR}/scripts'" \
        -ex "gef config pcustom.struct_path '${DIR}/structs'" \
        -ex "gef config syscall-args.path '${DIR}/syscall-tables'" \
        -ex "gef config context.libc_args True" \

commit 2066ca0e5d45cc316d073fcccd88cc2142b62cbf

Fix an incorrect scoped function call (introduced in #1093)

gef.py
@@ -9974,7 +9974,7 @@ class GefCommand(gdb.Command):
         corrected_settings_name: str = pane_name.replace(" ", "_")
         gef.config["context.layout"] += f" {corrected_settings_name}"
 
-        add_context_layout_mapping(corrected_settings_name, display_pane_function, pane_title_function, condition)
+        self.add_context_layout_mapping(corrected_settings_name, display_pane_function, pane_title_function, condition)
 
     def load(self) -> None:
         """Load all the commands and functions defined by GEF into GDB."""