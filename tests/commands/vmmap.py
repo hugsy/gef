ba70548a3ee24355c1c6daca0aa302d326dc2335
ValekoZ <lucas.valentin88@gmail.com>
2024-10-24 10:21:44 +0200

    [cmd] Add support for flags specifying filters type in vmmap, and allow multiple filters (#1120)
    Parse args:
     - `-a` / `--addr`:
    - filter by address -> parses the next arg as an int or asks gdb the
    value
     - `-n` / `--name`:
       - filter based on section name
    - If nothing is specified, print a warning and guess the type (previous
    behavior)
    
    ---------
    
    Co-authored-by: Grazfather <grazfather@gmail.com>

--- tests/commands/vmmap.py
+++ tests/commands/vmmap.py
@@ -20,7 +20,31 @@ class VmmapCommand(RemoteGefUnitTestGeneric):
         self.assertGreater(len(res.splitlines()), 1)
 
         res = gdb.execute("vmmap stack", to_string=True)
-        self.assertGreater(len(res.splitlines()), 1)
+        assert "`stack` has no type specified. We guessed it was a name filter." in res
+        self.assertEqual(len(res.splitlines()), 9)
 
         res = gdb.execute("vmmap $pc", to_string=True)
-        self.assertEqual(len(res.splitlines()), 2)
+        assert "`$pc` has no type specified. We guessed it was an address filter." in res
+        self.assertEqual(len(res.splitlines()), 8)
+
+    def test_cmd_vmmap_addr(self):
+        gef, gdb = self._gef, self._gdb
+        gdb.execute("start")
+
+        pc = gef.arch.register("pc")
+
+        res = gdb.execute(f"vmmap -a {pc:#x}", to_string=True)
+        self.assertEqual(len(res.splitlines()), 5)
+
+        res = gdb.execute("vmmap --addr $pc", to_string=True)
+        self.assertEqual(len(res.splitlines()), 5)
+
+    def test_cmd_vmmap_name(self):
+        gdb = self._gdb
+        gdb.execute("start")
+
+        res = gdb.execute("vmmap -n stack", to_string=True)
+        self.assertEqual(len(res.splitlines()), 5)
+
+        res = gdb.execute("vmmap --name stack", to_string=True)
+        self.assertEqual(len(res.splitlines()), 5)
