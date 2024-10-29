13ff22c341453c69dfc36b350b1f356bceecd0f6
Michael Krasnitski <42564254+mkrasnitski@users.noreply.github.com>
2024-10-29 10:46:16 -0700

    Apply review suggestion
    Co-authored-by: crazy hugsy <hugsy@users.noreply.github.com>

--- gef.py
+++ gef.py
@@ -1936,10 +1936,10 @@ def gef_pybytes(x: str) -> bytes:
 @lru_cache()
 def which(program: str) -> pathlib.Path:
     """Locate a command on the filesystem."""
-    try:
-        return pathlib.Path(shutil.which(program))
-    except TypeError:
+    res = shutil.which(program)
+    if not res:
         raise FileNotFoundError(f"Missing file `{program}`")
+    return pathlib.Path(res)
 
 
 def style_byte(b: int, color: bool = True) -> str:
