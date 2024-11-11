3bdba586cb5639dfeb1ea3266a13eaa0c4a57f54
crazy hugsy <hugsy@users.noreply.github.com>
2024-11-11 10:35:20 -0800

    Prevent duplicate panes in `context.layout` (#1153)
    ## Description
    
    Make sure panes added in `context.layout` have a unique name
    
    Fixes #1145

--- tests/commands/context.py
+++ tests/commands/context.py
@@ -13,4 +13,25 @@ class ContextCommand(RemoteGefUnitTestGeneric):
     cmd = "context"
 
 
-    # See https://github.com/hugsy/gef/projects/10
+    # TODO See https://github.com/hugsy/gef/projects/10
+
+    def test_duplicate_pane_name(self):
+        # Make sure we cannot have 2 context panes with an identical identifier
+        # See #1145 , #1153
+        gdb = self._gdb
+        gef = self._gef
+
+        new_pane_id = "new_pane1"
+        current_layout = gef.config["context.layout"]
+        assert not current_layout.endswith(new_pane_id)
+
+        res = gdb.execute(f"pi register_external_context_pane('{new_pane_id}', print, print, None)", to_string=True)
+        assert "Python Exception" not in res
+        new_layout = gef.config["context.layout"]
+        assert new_layout.endswith(new_pane_id)
+
+        res = gdb.execute(f"pi register_external_context_pane('{new_pane_id}', print, print, None)", to_string=True)
+        assert "Python Exception" not in res
+        new_layout2 = gef.config["context.layout"]
+        assert new_layout == new_layout2
+        assert f"Duplicate name for `{new_pane_id}`" in res
