1910a4a5e7730d4d7e2f46ad0a0ed2c7f2d56a37
hugsy <crazy.hugsy@outlook.com>
2024-11-11 17:07:25 -0800

    added arch

--- tests/extended/debian.sh
+++ tests/extended/debian.sh
@@ -8,10 +8,4 @@ rm -rf /var/lib/apt/lists/* && localedef -i en_US -c -f UTF-8 -A /usr/share/loca
 export LANG=en_US.UTF8
 export LC_ALL=en_US.UTF8
 
-git config --global --add safe.directory /gef
-cd /gef
-export PY_VER=`gdb-multiarch -q -nx -ex "pi print('.'.join(map(str, sys.version_info[:2])))" -ex quit`
-echo Using Python ${PY_VER}
-python${PY_VER} -m pip install --user --upgrade -r tests/requirements.txt -r docs/requirements.txt --break-system-packages
-make -C tests/binaries -j4
-python${PY_VER} -m pytest --forked -n 4 -v -m "not benchmark" tests/
+bash /gef/tests/extended/run_pytest.sh
--- tests/extended/fedora.sh
+++ tests/extended/fedora.sh
@@ -6,11 +6,4 @@ dnf install -y gdb cmake gcc python3 python3-pip procps file elfutils binutils c
 export LANG=en_US.UTF8
 export LC_ALL=en_US.UTF8
 
-git config --global --add safe.directory /gef
-cd /gef
-alias gdb-multiarch=gdb
-export PY_VER=`gdb-multiarch -q -nx -ex "pi print('.'.join(map(str, sys.version_info[:2])))" -ex quit`
-echo Using Python ${PY_VER}
-python${PY_VER} -m pip install --user --upgrade -r tests/requirements.txt -r docs/requirements.txt --break-system-packages
-make -C tests/binaries -j4
-python${PY_VER} -m pytest --forked -n 4 -v -m "not benchmark" tests/
+bash /gef/tests/extended/run_pytest.sh
