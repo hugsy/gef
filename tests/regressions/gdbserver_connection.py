a134540b05e06935d8ac3dc2e455748640ff5dae
hugsy <crazy.hugsy@outlook.com>
2025-03-15 09:07:58 -0700

    only disable when running on githubactions
    Signed-off-by: hugsy <crazy.hugsy@outlook.com>

--- tests/regressions/gdbserver_connection.py
+++ tests/regressions/gdbserver_connection.py
@@ -2,12 +2,13 @@ import pytest
 from tests.base import RemoteGefUnitTestGeneric
 from tests.utils import (
     ARCH,
+    IN_GITHUB_ACTIONS,
     gdbserver_session,
 )
 
 
 class RegressionGdbserverConnection(RemoteGefUnitTestGeneric):
-    @pytest.mark.skipif(ARCH not in ("x86_64",), reason=f"Skipped for {ARCH}")
+    @pytest.mark.skipif(ARCH == "aarch64" and IN_GITHUB_ACTIONS, reason=f"Skipped for {ARCH}")
     def test_can_establish_connection_to_gdbserver_again_after_disconnect(self):
         """Ensure that gdb can connect to a gdbserver again after disconnecting (PR #896)."""
         gdb = self._gdb
